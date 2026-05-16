0df8f435433067af433078fab73da06fda912443
Piyush Saxena <103770082+Raxile@users.noreply.github.com>
2026-05-17 01:27:43 +0530

    Add: two-integer-sum-ii - submission-4

---
+++ Data Structures & Algorithms/two-integer-sum-ii/submission-4.js
@@ -0,0 +1,25 @@
+class Solution {
+    /**
+     * @param {number[]} numbers
+     * @param {number} target
+     * @return {number[]}
+     */
+    twoSum(numbers, target) {
+ const obj = {}
+    for(let i = 0; i > numbers.length-1-i || i < numbers.length; i++) {
+        const leftPartner = target - numbers[i]
+        const rightPartner = target - numbers[numbers.length-1-i]
+        
+        if(leftPartner in obj&&obj[leftPartner]+1 !==i+1) {           
+            return [obj[leftPartner]+1, i+1].sort()  
+        }
+        if(rightPartner in obj&&obj[rightPartner]+1 !==numbers.length-i){
+            return [obj[rightPartner]+1, numbers.length-i].sort()  
+        }
+        obj[numbers[i]] = i  
+        obj[numbers[numbers.length-1-i]] = numbers.length-1-i
+    }
+    return []
+    }
+}
+
