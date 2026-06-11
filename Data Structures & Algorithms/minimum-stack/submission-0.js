b6f1836bed403220bef2d599f2ddc7508ec863c6
Piyush Saxena <103770082+Raxile@users.noreply.github.com>
2026-06-12 00:29:31 +0530

    Add: minimum-stack - submission-0

---
+++ Data Structures & Algorithms/minimum-stack/submission-0.js
@@ -0,0 +1,41 @@
+class MinStack {
+    constructor() {
+        this.stack = [];
+        this.minStack = [];
+    }
+
+    /**
+     * @param {number} val
+     * @return {void}
+     */
+    push(val) {
+        this.stack.push(val)
+    const currentMin = this.minStack.length === 0 
+        ? val  // ✅ first element is always min
+        : Math.min(this.minStack[this.minStack.length-1], val)
+
+        this.minStack.push(currentMin)
+    }
+
+    /**
+     * @return {void}
+     */
+    pop() {
+        this.stack.pop()
+        this.minStack.pop()
+    }
+
+    /**
+     * @return {number}
+     */
+    top() {
+        return this.stack[this.stack.length-1];
+    }
+
+    /**
+     * @return {number}
+     */
+    getMin() {
+        return this.minStack[this.minStack.length-1];
+    }
+}
