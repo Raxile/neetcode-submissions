6aa10555d1bbbf61712f31472b6cd04bd5be60d9
Piyush Saxena <103770082+Raxile@users.noreply.github.com>
2026-08-03 09:03:49 +0530

    Add: copy-linked-list-with-random-pointer - submission-1

---
+++ Data Structures & Algorithms/copy-linked-list-with-random-pointer/submission-1.js
@@ -0,0 +1,35 @@
+// class Node {
+//   constructor(val, next = null, random = null) {
+//       this.val = val;
+//       this.next = next;
+//       this.random = random;
+//   }
+// }
+
+class Solution {
+    /**
+     * @param {Node} head
+     * @return {Node}
+     */
+copyRandomList(head) {
+    const map = new Map()
+    map.set(null, null)  // handle null case!
+
+    // pass 1: create all copies
+    let curr = head
+    while(curr) {
+        map.set(curr, new Node(curr.val))  // node→copy
+        curr = curr.next
+    }
+
+    // pass 2: set next and random
+    curr = head
+    while(curr) {
+        map.get(curr).next = map.get(curr.next)      // ✅
+        map.get(curr).random = map.get(curr.random)  // ✅
+        curr = curr.next
+    }
+    return map.get(head)
+    
+}
+}
