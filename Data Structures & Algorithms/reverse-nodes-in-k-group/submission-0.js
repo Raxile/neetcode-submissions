5b4872377e9b8c3af1a04df9d59496d03d61e8b6
Piyush Saxena <103770082+Raxile@users.noreply.github.com>
2026-08-07 00:40:01 +0530

    Add: reverse-nodes-in-k-group - submission-0

---
+++ Data Structures & Algorithms/reverse-nodes-in-k-group/submission-0.js
@@ -0,0 +1,39 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     constructor(val = 0, next = null) {
+ *         this.val = val;
+ *         this.next = next;
+ *     }
+ * }
+ */
+
+class Solution {
+    /**
+     * @param {ListNode} head
+     * @param {number} k
+     * @return {ListNode}
+     */
+reverseKGroup(head, k) {
+    let count = 0
+    let node = head
+    while(node && count < k) {
+        node = node.next
+        count++
+    }
+
+    if(count < k) return head  // ✅
+
+    let prev = null
+    let curr = head
+    for(let i = 0; i < k; i++) {
+        const next = curr.next
+        curr.next = prev
+        prev = curr
+        curr = next
+    }
+
+    head.next = this.reverseKGroup(curr, k)  // ✅ connect to next group
+    return prev  // ✅ new head!
+}
+}
