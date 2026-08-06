993d16ae82e633702d163d4463d0938733a08379
Piyush Saxena <103770082+Raxile@users.noreply.github.com>
2026-08-07 00:55:40 +0530

    Add: is-palindrome - submission-15

---
+++ Data Structures & Algorithms/is-palindrome/submission-15.js
@@ -0,0 +1,25 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {boolean}
+     */
+    isPalindrome(s) {
+        const str = s.toLocaleLowerCase().replace(/[^a-zA-Z0-9]/g, "");
+        let left=0;
+        let right = str.length-1;
+        let isPalendrome = true
+
+         while(left<=right){
+            if(str[left]!==str[right]){
+                isPalendrome = false;
+                break;
+            }
+
+            left++;
+            right--;
+         }
+
+
+        return isPalendrome
+    }
+}
